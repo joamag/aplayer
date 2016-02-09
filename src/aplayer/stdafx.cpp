6f96fbbfe6ead76620d992a006df79dce1fc3c51
joamag <joamag@gmail.com>
2016-02-09 22:46:23 +0000

    new license values

--- src/aplayer/stdafx.cpp
+++ src/aplayer/stdafx.cpp
@@ -5,24 +5,24 @@
  This file is part of Hive Audio Player.
 
  Hive Audio Player is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+ it under the terms of the Apache License as published by the Apache
+ Foundation, either version 2.0 of the License, or (at your option) any
+ later version.
 
  Hive Audio Player is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
+ Apache License for more details.
 
- You should have received a copy of the GNU General Public License
- along with Hive Audio Player. If not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the Apache License along with
+ Hive Audio Player. If not, see <http://www.apache.org/licenses/>.
 
  __author__    = João Magalhães <joamag@hive.pt>
  __version__   = 1.0.0
  __revision__  = $LastChangedRevision$
  __date__      = $LastChangedDate$
  __copyright__ = Copyright (c) 2008-2016 Hive Solutions Lda.
- __license__   = GNU General Public License (GPL), Version 3
+ __license__   = Apache License, Version 2.0
 */
 
 #include "stdafx.h"
